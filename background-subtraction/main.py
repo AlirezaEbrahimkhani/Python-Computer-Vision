d13ee18a75c60f563ca4bc4e2bb3b4d4cfc616be
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-11-19 23:17:36 +0330

    [Feat] fix subtraction bug

--- background-subtraction/main.py
+++ background-subtraction/main.py
@@ -1,39 +1,32 @@
-from __future__ import print_function
-import cv2 as cv
 import numpy as np
+import cv2 as cv
 
+backSub = cv.createBackgroundSubtractorMOG2()
 system_camera = cv.VideoCapture(0)
 
-if not (system_camera.isOpened()):
-    print("Could not open video device")
+if not system_camera.isOpened():
+    print('Unable to open camera')
+    exit(0)
 
-system_camera.set(cv.CAP_PROP_FRAME_WIDTH, 640)
-system_camera.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
-
-
-while(True):
-    # Create Background Subtractor objects
-    backSub = cv.createBackgroundSubtractorMOG2()
-    # Capture frame-by-frame
+while True:
     ret, frame = system_camera.read()
-    
-    # Update the background model
+    if frame is None:
+        break
+
     fgMask = backSub.apply(frame)
 
-    # Get the frame number and write it on the current frame
     cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
     cv.putText(frame, str(system_camera.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
                cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-    
+
     # Apply median filter
     median = cv.medianBlur(fgMask, 5)
     gauss = cv.GaussianBlur(fgMask, (5, 5), 0)
-    median_filter_frame = np.concatenate((median, gauss), axis=1)
 
-    # Display the resulting frame
     cv.imshow('Frame', frame)
     cv.imshow('FG Mask', fgMask)
-    cv.imshow('Median Filter Apply', median_filter_frame)
+    cv.imshow('Gaussian Filter applied', gauss)
+    cv.imshow('Median Filter applied', median)
 
     # Waits for a user input to quit the application
     if cv.waitKey(1) & 0xFF == ord('q'):
